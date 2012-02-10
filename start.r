933d6699a7a69db8184a4931d1b3c7123dfaf434
Oldes <Oldes.Huhuman@gmail.com>
2012-02-10 13:21:43 +0100

    Added helper minifunctions into startup

--- start.r
+++ start.r
@@ -3,7 +3,12 @@ REBOL [
 ]
 
 with: func[obj body][do bind body obj]
+drc:  does[do read clipboard://]
+tm:   func[count [integer!] code [block!] /local t][t: now/time/precise loop count code probe now/time/precise - t]
+ls: :list-dir
+cd: :change-dir
 
+dir_lib:               ;used to store external libraries
 dir_imagemagick: %lib/ ;used by imagick to load MagickWand's dll or the convert app;
 
 do %rs.r
