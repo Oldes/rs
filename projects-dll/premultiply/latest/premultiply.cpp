44af60aee53c7ac334e33a0a517480a3db3b9e5d
Oldes <Oldes.Huhuman@gmail.com>
2012-02-20 15:35:38 +0100

    included demultiply in the premultiply dll

--- projects-dll/premultiply/latest/premultiply.cpp
+++ projects-dll/premultiply/latest/premultiply.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-#define WINDOWS
-
-#include <windows.h>
-
+inline unsigned char asChar(unsigned int i)
+ {
+   return 255 < i ? 255 : i;
+ }
      
 extern "C" __declspec(dllexport)
       void premultiply(unsigned char *buf, int len)
 	  {
-		int a;
-		int n;
+		unsigned int a;
+		unsigned int n;
 
 		for (n = 0; n < len; n++) {
 			a = buf[n]; n++;
@@ -18,4 +18,20 @@ extern "C" __declspec(dllexport)
 			buf[n] = (unsigned char)((buf[n] * a) / 255);;
 		}
 	  }
+extern "C" __declspec(dllexport)
+      void demultiply(unsigned char *buf, int len)
+	  {
+		unsigned int a;
+		unsigned int n;
 
+		for (n = 0; n < len; n++) {
+			if(buf[n]==0){
+				n+=3;
+			} else {
+				a = buf[n] << 8; n++;
+				buf[n] = asChar((buf[n] << 16) / a); n++;
+				buf[n] = asChar((buf[n] << 16) / a); n++;
+				buf[n] = asChar((buf[n] << 16) / a);
+			}
+		}
+	  }
--- projects-dll/premultiply/latest/premultiply.r
+++ projects-dll/premultiply/latest/premultiply.r
@@ -11,17 +11,21 @@ REBOL [
     Needs: none
     Tabs: none
     Usage: [
-		argb: #{
+		;rs/run/usage 'premultiply
+		system/options/binary-base: 32
+		print ((probe demultiply #{5522ffaa}) = (probe demultiply2 #{5522ffaa}))
+		print ((probe demultiply #{000000005522ffaa}) = (probe demultiply2 #{000000005522ffaa}))
+		ARGB: #{
 		00000000000000000000000000000000238A9292D7232724ED000000AD171914
 		A3999E9E9AD6DBD881DADADC77BDBDC5765F5F637783837E79CFCFCF80ACB4B8
 		96332C33A92A2727B15E6262A25D6565C7383C40FF0305069E515451156D6D6D}
-		argb-ok: #{
+		ARGB-ok: #{
 		0000000000000000000000000000000023121414D71D201EED000000AD0F100D
 		A36164649A818482816E6E6F7758585B762B2B2D773D3D3A7962626280565A5C
 		961E191EA91B1919B1414444A23B4040C72B2E31FF0305069E32343215080808}
 
-		print argb-ok = premultiply  copy argb
-		print argb-ok = premultiply2 copy argb
+		print ARGB-ok = premultiply  copy ARGB
+		print ARGB-ok = premultiply2 copy ARGB
 	]
     Purpose: none
     Comment: none
@@ -38,11 +42,12 @@ REBOL [
 ctx-premultiply: context [
 	*lib: load/library dir_lib/premultiply.dll
 	r_premultiply: make routine! [*data [integer!] bytes [integer!]] *lib "premultiply"
-	set 'premultiply func[data [binary!] /local bytes][
-		bytes: length? data
+	r_demultiply:  make routine! [*data [integer!] bytes [integer!]] *lib "demultiply"
+	set 'premultiply func[ARGB [binary!] /local bytes][
+		bytes: length? ARGB
 		if 0 <> (bytes // 4) [make error! "invalid data size to premultiply"]
-		r_premultiply address? data bytes
-		data
+		r_premultiply address? ARGB bytes
+		ARGB
 	]
 	set 'premultiply2 func[ARGB /local a bytes][
 		bytes: length? ARGB
@@ -53,6 +58,34 @@ ctx-premultiply: context [
 			ARGB: change ARGB to char! (a * ARGB/1)
 			ARGB: change ARGB to char! (a * ARGB/1)
 			ARGB: change ARGB to char! (a * ARGB/1)
+		]
+		ARGB: head ARGB
+	]
+	
+	set 'demultiply func[ARGB [binary!] /local bytes][
+		bytes: length? ARGB
+		if 0 <> (bytes // 4) [make error! "invalid data size to demultiply"]
+		r_demultiply address? ARGB bytes
+		ARGB
+	]
+	set 'demultiply2 func[ARGB [binary!] /local bytes a][
+		bytes: length? ARGB
+		if 0 <> (bytes // 4) [make error! "invalid data size to demultiply"]
+		loop bytes / 4 [
+			either 0 = a: first ARGB [
+				ARGB: skip ARGB 4
+			][
+				if error? try [
+					a: a * 256
+					ARGB: next ARGB
+					ARGB: change ARGB to char! min 255 to integer! (shift/left first ARGB 16) / a
+					ARGB: change ARGB to char! min 255 to integer! (shift/left first ARGB 16) / a
+					ARGB: change ARGB to char! min 255 to integer! (shift/left first ARGB 16) / a
+				][
+					print ["ERR:" a mold copy/part ARGB 4]
+					halt
+				]
+			]
 		]
 		ARGB: head ARGB
 	]
