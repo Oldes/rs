477b747f0a69277ed344d308d928bb02fa73d6f0
Oldes <Oldes.Huhuman@gmail.com>
2012-02-20 15:38:39 +0100

    bbcode.r review

--- projects/bbcode/latest/bbcode.r
+++ projects/bbcode/latest/bbcode.r
@@ -1,61 +1,90 @@
 REBOL [
     Title: "BBcode"
-    Date: 5-Jan-2009/1:06:26+1:00
+    Date: 19-Feb-2012/13:00:39+1:00
     Name: 'bbcode
     Version: 0.1.0
+	History: [
+		0.1.0  5-Jan-2009 "initial version"
+		0.2.0 19-Feb-2012 "review"
+	]
     File:    %bbcode.r
     Author:  "David 'Oldes' Oliva"
     Email:   oliva.david@seznam.cz
-    Home:    http://box.lebeda.ws/~hmm/
+    Home:    http://rebol.desajn.net
     Owner: none
     Rights: none
     Needs: none
     Tabs: 4
     Usage: [
     	test-cases: [
-			{text [b]bold[/b] abc}  {text <b>bold</b> abc}
-			{text [b]bold [i]italic[/b]}  {text <b>bold <i>italic</i></b>}
-			{[s]strikethrough text[/s]}  {<s>strikethrough text</s>}
-			{[url]http://example.org[/url]}  {<a href="http://example.org">http://example.org</a>}
-			{[url=http://example.com]Example[/url]}  {<a href="http://example.com">Example</a>}
-			{[url=http://example.com][b]Example[/url]}  {<a href="http://example.com"><b>Example</b></a>}
-			{[b][ul][li]Jenny[/li][li]Alex[/li][li]Beth[/li][/ul][/b]}  {<b><ul><li>Jenny</li><li>Alex</li><li>Beth</li></ul></b>}
-			{[ul][li]bla[li]bla}  {<ul><li>bla</li><li>bla</li></ul>}
-			{[ul][li][b]bla[li]bla}  {<ul><li><b>bla</b></li><li>bla</li></ul>}
+			{text [b]bold[/b] abc}             {<p>text <b>bold</b> abc</p>}
+			{text [b]bold [i]italic[/b]}       {<p>text <b>bold <i>italic</i></b></p>}
+			{[s]strikethrough text[/s]}        {<p><s>strikethrough text</s></p>}
+			{[url]http://example.org[/url]}    {<p><a href="http://example.org">http://example.org</a></p>}
+			{[url=http://example]Test[/url]}   {<p><a href="http://example">Test</a></p>}
+			{[url=http://example][b]Test[/url]}{<p><a href="http://example"><b>Test</b></a></p>}
+			{[b][ul][li]Jenny[/li][li]Alex[/li][li]Beth[/li][/ul][/b]}
+                                               {<p><b><ul><li>Jenny</li><li>Alex</li><li>Beth</li></ul></b></p>}
+			{[ul][li]bla[li]bla}               {<ul><li>bla</li><li>bla</li></ul>}
+			{text[ul][li]bla[li]bla}           {<p>text</p><ul><li>bla</li><li>bla</li></ul>}
+			{[ul][li][b]bla[li]bla}            {<ul><li><b>bla</b></li><li>bla</li></ul>}
 			{[ul][li]bla[li][ol][li]bla[/ol]}  {<ul><li>bla</li><li><ol><li>bla</li></ol></li></ul>}
-			{[code]xx[b]yy[/b]zz[/code]}  {<code>xx[b]yy[/b]zz</code>}
-			{[list][*]aaa[*]bbb[/list]}  {<ul><li>aaa</li><li>bbb</li></ul>}
-			{[list=a][*]aaa[*]bbb[/list]}  {<ol style="list-style-type: lower-alpha;"><li>aaa</li><li>bbb</li></ol>}
-			{[list=A][*]aaa[*]bbb[/list]}  {<ol style="list-style-type: upper-alpha;"><li>aaa</li><li>bbb</li></ol>}
-			{[/b]} {}
-			{aa[b="]bb} {aa[b="]bb}
-			{[quote]blabla} {<fieldset><blockquote>blabla</blockquote></fieldset>}
-			{[quote=Carl]blabla} {<fieldset><legend>Carl</legend><blockquote>blabla</blockquote></fieldset>}
-			{[img]http://www.google.com/intl/en_ALL/images/logo.gif[/img]} {<img src="http://www.google.com/intl/en_ALL/images/logo.gif" alt="">}
+			{[ul][*]bla^/^/bla} {<ul><li><p>bla</p>^/^/<p>bla</p></li></ul>}
+			{[ul][*]bla[*]bla}  {<ul><li>bla</li><li>bla</li></ul>}
+			{[code]xx[b]yy[/b]zz[/code]}       {<p><code>xx[b]yy[/b]zz</code></p>}
+			{[list][*]aaa[*]bbb[/list]}        {<ul><li>aaa</li><li>bbb</li></ul>}
+			{[list=a][*]aaa[*]bbb[/list]}      {<ol style="list-style-type: lower-alpha;"><li>aaa</li><li>bbb</li></ol>}
+			{[list=A][*]aaa[*]bbb[/list]}      {<ol style="list-style-type: upper-alpha;"><li>aaa</li><li>bbb</li></ol>}
+			{[/b]}                             {<p></p>}
+			{aa[b="]bb}                        {<p>aa[b="]bb</p>}
+			{[quote]blabla}                    {<p><fieldset><blockquote>blabla</blockquote></fieldset></p>}
+			{[quote=Carl]blabla}               {<p><fieldset><legend>Carl</legend><blockquote>blabla</blockquote></fieldset></p>}
+			{[img]http://www.google.com/intl/en_ALL/images/logo.gif[/img]}
+                                               {<p><img src="http://www.google.com/intl/en_ALL/images/logo.gif" alt=""></p>}
 			{[url=http://http://www.google.com/][img]http://www.google.com/intl/en_ALL/images/logo.gif[/url][/img]}
-			 {<a href="http://http://www.google.com/"><img src="http://www.google.com/intl/en_ALL/images/logo.gif" alt=""></a>}
-			{[img]1.gif [img]2.gif} {<img src="1.gif" alt=""> <img src="2.gif" alt="">}
-			{text [size=tiny]tiny} {text <span style="font-size: xx-small;">tiny</span>}
-			{[h1]header[/h1]} {<h1>header</h1>}
-			{[color]ee[/color][color=#F00]red[color=#00FF00]green} {ee<span style="color: #F00;">red<span style="color: #00FF00;">green</span></span>}
-			{<a>}  {&lt;a>}
-			{multi^/line}  {multi^/<br>line}
-			{invalid [size]size[/size]}  {invalid <span>size</span>}
-			{[align=right]right}  {<div style="text-align: right;">right</div>}
-			{[email]x@x.cz[/email] [email=x@x.cz]email [b]me[/email]}  {<a href="mailto:x@x.cz">x@x.cz</a> <a href="mailto:x@x.cz">email <b>me</b></a>}
+			                                   {<p><a href="http://http://www.google.com/"><img src="http://www.google.com/intl/en_ALL/images/logo.gif" alt=""></a></p>}
+			{[img]1.gif [img]2.gif}            {<p><img src="1.gif" alt=""> <img src="2.gif" alt=""></p>}
+			{text [size=tiny]tiny}             {<p>text <span style="font-size: xx-small;">tiny</span></p>}
+			{[h1]header[/h1]}                  {<h1>header</h1>}
+			{[color]ee[/color][color=#F00]red[color=#00FF00]green}
+                                               {<p>ee<span style="color: #F00;">red<span style="color: #00FF00;">green</span></span></p>}
+			{<a>}                              {<p>&lt;a></p>}
+			{multi^/line}                      {<p>multi^/line</p>}
+			{invalid [size]size[/size]}        {<p>invalid <span>size</span></p>}
+			{[align=right]right}               {<p><div style="text-align: right;">right</div></p>}
+			{[email]x@x.cz[/email] [email=x@x.cz]email [b]me[/email]}
+                                               {<p><a href="mailto:x@x.cz">x@x.cz</a> <a href="mailto:x@x.cz">email <b>me</b></a></p>}
+
+			{[u]underlined[/u]}                {<p><u>underlined</u></p>}
+			{[h3=underline]Portréty [b]X}      {<h3 class="underline">Portréty <b>X</b></h3>}
+			{[img=680x300]pozvanka.jpg}        {<p><img width=680 height=300 src="pozvanka.jpg" alt=""></p>}
+			{[img size="680x300" alt='pozvanka na "vystavu"']pozvanka.jpg}
+                                               {<p><img width=680 height=300 src="pozvanka.jpg" alt="pozvanka na &quot;vystavu&quot;"></p>}
+			{[img width="680" height="300" alt="pozvanka na vystavu"]pozvanka.jpg}
+                                               {<p><img width=680 height=300 src="pozvanka.jpg" alt="pozvanka na vystavu"></p>}
+			;{[img resize=680x0]pozvanka.jpg[/]} {<img width=680 height=300 src="pozvanka.jpg" alt="">}
+			{[h2=title]stylised title[/h2]}    {<h2 class="title">stylised title</h2>}
+			{text^/on one line ^/second line}  {<p>text^/on one line <br>^/second line</p>}
+			{par 1^/^/par 2}                   {<p>par 1</p>^/^/<p>par 2</p>}
+			{[script][/script]}                {<p>[script]</p>}
+			{[url=http://][h1=]bla}            {<p><a href="http://"><h1>bla</h1></a></p>}
 		]
+		xtest-cases: [
+			
+		]
+		errors: copy []
 		foreach [src result] test-cases [
 			print ["<==" src]
 			print ["==>" tmp: bbcode src]
-			print either tmp = result ["OK"][join "ERR " result]
+			print either tmp = result ["OK"][repend/only errors [src tmp] join "ERR " result]
 			print "---"
+		]
+		unless empty? errors [
+			print ["ERRORS:" mold new-line/all errors true]
 		]
 	]
     Purpose: {Basic BBCode implementation. For more info about BBCode check http://en.wikipedia.org/wiki/BBCode}
     Comment: none
-    History: [
-    	0.1.0 5-Jan-2009 "Initial version"
-	]
 	Type: [tool dialect function]
     Library: [
         level:    'advanced
@@ -68,20 +97,64 @@ REBOL [
 ]
 
 ctx-bbcode: context [
-	ch-normal:    complement charset "[<^/"
-	ch-attribute: complement charset {"'<>]}
-	ch-hexa: charset [#"a" - #"f" #"A" - #"F" #"0" - #"9"]
-	ch-name: charset [#"a" - #"z" #"A" - #"Z" #"*" #"0" - #"9"]
-	ch-url:  charset [#"a" - #"z" #"A" - #"Z" #"0" - #"9" "./:~+-%#\_=&?@"]
+	ch_normal:    complement charset "[<^M^/"
+	ch_space: charset [#"^A" - #" " #"^(7F)"]
+	ch_attribute:  complement charset {"'<>]}
+	ch_attribute1: complement charset {'<>]}
+	ch_attribute2: complement charset {"<>]}
+	ch_attribute3: union complement ch_space ch_attribute
+	ch_hexa:  charset [#"a" - #"f" #"A" - #"F" #"0" - #"9"]
+	ch_name:  charset [#"a" - #"z" #"A" - #"Z" #"*" #"0" - #"9"]
+	ch_url:   charset [#"a" - #"z" #"A" - #"Z" #"0" - #"9" "./:~+-%#\_=&?@"]
+	ch_safe-value-chars: complement charset {'"}
+	
 	opened-tags: copy []
-	rl-attribute: [#"=" copy attr any ch-attribute]
-
+	rl_newline: ["^M^/" | "^/"]
+	rl_attribute: [any ch_space #"=" copy attr any ch_attribute]
+	rl_attributes: [
+		rl_attribute
+		|
+		(clear attributes)
+		some [
+			any ch_space
+			copy tmp some ch_name any ch_space #"=" any ch_space [
+				#"^"" copy attr any ch_attribute2 #"^""
+				|
+				#"'"  copy attr any ch_attribute1 #"'"
+				|
+				copy attr any ch_attribute3
+			] any ch_space
+			(
+				repend attributes [tmp attr]
+				tmp: attr: none
+			)
+		]
+	]
 	allow-html-tags?: false
 	attr: none
+	attributes: make hash! 20
 	html: copy ""
-			
-	close-tags: func[tags [block!]][
-		foreach tag head reverse tags [
+	
+	get-attribute: func[name /local tmp][all [tmp: select/skip attributes name 2 encode-value first tmp]]
+	encode-value: func[value [any-string!] /local out tmp][
+		out: copy ""
+		parse/all value [
+			any [
+				pos: ;(probe pos)
+				[ 
+					#"'" (append out "&apos;") |
+					#"^"" (append out "&quot;")
+				]
+				|
+				copy tmp some ch_safe-value-chars (append out tmp)
+			]
+		]
+		out
+	]
+	close-tags: func[/only to-tag][
+		opened-tags: tail opened-tags
+		while [not empty? opened-tags: back opened-tags][
+			tag: opened-tags/1
 			append html case [
 				tag = "url" ["</a>"]
 				find ["list" "color" "quote" "size" "align" "email"] tag [""]
@@ -90,7 +163,69 @@ ctx-bbcode: context [
 					rejoin ["</" tag ">"]
 				]
 			]
+			remove opened-tags
+			if tag = to-tag [break]
 		]
+		opened-tags: head opened-tags
+	]
+	form-size: func[/local size out][
+		out: copy ""
+		case/all [
+			all [
+				none? attr
+				empty? attributes
+			][	return out ]
+			
+			any [
+				all [
+					size: get-attribute "size"
+					not error? try [size: to pair! size]
+				]
+				all [
+					attr
+					not error? try [size: to pair! attr]
+					size <> 0x0
+				]
+			][
+				return rejoin [" width=" size/x " height=" size/y]
+			]
+			
+			all [
+				size: get-attribute "resize"
+				not error? try [size: to pair! size]
+			][
+				return rejoin [" width=" size/x " height=" size/y]
+			]
+			
+			not error? try [
+				size: to integer! get-attribute "width"
+			][
+				append out rejoin [" width=" size]
+			]
+			
+			not error? try [
+				size: to integer! get-attribute "height"
+			][
+				append out rejoin [" height=" size]
+			]
+		]
+		any [out ""]
+	]
+	
+	close-p-if-possible: func[ /local p] [
+		if "p" = probe last opened-tags [
+			close-tags/only "p"
+			if "<p></p>" = p: skip tail html -7 [
+				clear p
+			]
+		]
+	]
+	emit-tag-p: does [
+		append html "<p>"
+		append opened-tags "p"
+	]
+	emit-tag: func[tag][
+		insert tail html either block? tag [rejoin tag][tag]
 	]
 	
 	enabled-tags: [
@@ -99,39 +234,43 @@ ctx-bbcode: context [
 		"color" "quote" "img" "size" "rebol" "align" "email"
 	]
 	
-	set 'bbcode func["Converts BBCode markup into HTML" code [string! binary! file! url!] "Input with BBCode tags" /local tmp tag][
+	set 'bbcode func["Converts BBCode markup into HTML" code [string! binary! file! url!] "Input with BBCode tags" /local tmp tag pos][
 		clear html
 		if any [file? code url? code][code: read/binary code]
+		emit-tag-p
 		parse/all code [
 			any [
 				(attr: none)
-				copy tmp some ch-normal (append html tmp)
+				copy tmp some ch_normal (append html tmp)
 				|
-				"[url]" copy tmp some ch-url opt "[/url]" (
-					append html rejoin [{<a href="} tmp {">} tmp {</a>}]
+				"[url]" copy tmp some ch_url opt "[/url]" (
+					emit-tag [{<a href="} encode-value tmp {">} tmp {</a>}]
 				)
 				|
-				"[email]" copy tmp some ch-url opt "[/email]" (
-					append html rejoin [{<a href="mailto:} tmp {">} tmp {</a>}]
+				"[email]" copy tmp some ch_url opt "[/email]" (
+					emit-tag [{<a href="mailto:} encode-value tmp {">} tmp {</a>}]
 				)
 				|
-				"[img]" copy tmp some ch-url opt "[/img]" (
-					append html rejoin [{<img src="} tmp {" alt="">}]
-					
+				"[img" opt rl_attributes #"]" copy tmp some ch_url opt "[/img]" (
+					emit-tag [{<img} form-size { src="} encode-value tmp {" alt="} any [get-attribute "alt" ""] {">}]
 				) 
 				|
 				"[code]" copy tmp to "[/code]" thru "]" (
-					append html rejoin [{<code>} tmp {</code>}]
+					emit-tag [{<code>} tmp {</code>}]
 				)
 				|
 				"[rebol]" copy tmp to "[/rebol]" thru "]" (
-					append html rejoin [{<code>} tmp {</code>}]
+					emit-tag [{<code>} tmp {</code>}]
 					;TODO: add REBOL code colorizer
+				)
+				|
+				"[/]" (
+					close-tags
 				)
 				|
 				#"[" [
 					;normal opening tags
-					copy tag some ch-name opt rl-attribute			
+					copy tag some ch_name opt rl_attributes			
 					#"]" (
 						if tag = "*" [tag: "li"]
 						append html either find enabled-tags tag [
@@ -142,16 +281,18 @@ ctx-bbcode: context [
 									none? find tmp "ol"
 									none? find tmp "ul"
 								][
-									close-tags copy tmp
-									clear tmp
+									close-tags/only tag
 								]
 							]
-							append opened-tags tag
+							
 							switch/default tag [
-								"url"  [rejoin [{<a href="} attr {">}]]
+								"url"  [
+									append opened-tags "a"
+									rejoin [{<a href="} encode-value attr {">}]
+								]
 								"color" [
 									either all [attr parse attr [
-										#"#" [6 ch-hexa | 3 ch-hexa]
+										#"#" [6 ch_hexa | 3 ch_hexa]
 									]][
 										append opened-tags "span"
 										rejoin [{<span style="color: } attr {;">}]
@@ -173,6 +314,7 @@ ctx-bbcode: context [
 								]
 								"list" [
 									if none? attr [attr: ""]
+									close-p-if-possible
 									parse/case attr [
 										[
 											  "a" (tmp: {<ol style="list-style-type: lower-alpha;">})
@@ -221,11 +363,17 @@ ctx-bbcode: context [
 								"email" [
 									either error? try [tmp: to-email attr][""][
 										append opened-tags "a"
-										rejoin [{<a href="mailto:} tmp {">}]
+										rejoin [{<a href="mailto:} encode-value tmp {">}]
 									]
 								]
 							][
-								rejoin ["<" tag ">"]
+								if find ["h1" "h2" "h3" "h4" "h5" "ul" "ol"] tag [
+									close-p-if-possible
+								]
+								append opened-tags tag
+								rejoin either none? attr [
+									["<" tag ">"]
+								][	["<" tag { class="} encode-value attr {">}] ]
 							]
 							
 						][
@@ -236,11 +384,10 @@ ctx-bbcode: context [
 					|
 					;closing tags
 					#"/" 
-					copy tag some ch-name
+					copy tag some ch_name
 					#"]" (
 						either tmp: find/last opened-tags tag [
-							close-tags copy tmp
-							clear tmp
+							close-tags/only tag
 						][
 							;;unopened tag, hidden by default, uncomment next line if you don't want to hide it
 							;append html rejoin [{[/} tag {]}] 
@@ -251,12 +398,37 @@ ctx-bbcode: context [
 				|
 				#"<" (append html either allow-html-tags? ["<"]["&lt;"])
 				|
-				#"^/" (append html "^/<br>")
+				2 rl_newline (
+					either any [
+						pos: find/last opened-tags "ul"
+						pos: find/last opened-tags "ol"
+					][
+						if all [
+							pos: find/last pos "li"
+							none? find pos "p"
+						][
+							insert next pos "p" 
+							insert find/last/tail html "<li>" "<p>"
+							close-tags/only "p"
+						]
+					][
+						close-tags/only "p"
+					]
+					
+					append html "^/^/"
+					emit-tag-p
+				)
+				|
+				pos: rl_newline (
+					case [
+						#" " = pos/(-1) [append html "<br>^/"]
+						true [append html lf]
+					]
+				)
 			]
 		]
 		unless empty? opened-tags [
-			close-tags opened-tags
-			clear opened-tags
+			close-tags
 		]
 		copy html
 	]
