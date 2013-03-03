c256625b0275baeaf742ac70d7102cb34dad8308
Oldes <oldes.huhuman@gmail.com>
2013-03-03 18:50:16 +0100

    xfl - fixed path typo in resizing xfl rules

--- projects-rswf/xfl/latest/rules_resize.r
+++ projects-rswf/xfl/latest/rules_resize.r
@@ -3,7 +3,7 @@ rebol []
 resize-image: func[src trg percent /name fname][
 	src-size: get-image-size src
 	if 0 <> call/wait probe ajoin [
-		to-local-file dir_imagemagick "convert"
+		to-local-file dir_imagemagick "\convert"
 		{ "} to-local-file src {"}
 		;" -resize " form (100 * percent/1) "%x" form (100 * percent/2) "%!"
 		" -resize " form (round/ceiling(percent/1 * src-size/x)) "x" form (round/ceiling(percent/2 * src-size/y)) "!"
@@ -30,7 +30,7 @@ resize-image-android: func[src trg percent /name fname /local filename tmp src-s
 	
 	
 	if 0 <> call/wait probe ajoin [
-		to-local-file dir_imagemagick "convert"
+		to-local-file dir_imagemagick "\convert"
 		{ "} to-local-file src {"}
 		;" -resize " form (100 * percent/1) "%x" form (100 * percent/2) "%!"
 		" -resize " form (round/ceiling(percent/1 * src-size/x)) "x" form (round/ceiling(percent/2 * src-size/y)) "!"
@@ -154,7 +154,8 @@ append XFL-action-rules [
 			"morphSegments" |
 			"textRuns" |
 			"DOMTextRun" |
-			"textAttrs"	
+			"textAttrs"	|
+			"motionObjectXML"
 		]  (
 			parse-xfl content
 		) |
