ab13da054b6eaeb43d27ace9b9e65bc4b11edc82
Oldes <oldes.huhuman@gmail.com>
2014-04-11 13:14:04 +0200

    start.r - some local changes, you can ignore this

--- start.r
+++ start.r
@@ -12,10 +12,18 @@ dir_lib:         join what-dir %lib/     ;used to store external libraries
 
 
 ;Change this variable to your ImageMagick installation location if needed
-dir_imagemagick: %"/c/Program Files (x86)/ImageMagick-6.7.5-Q16/"
+dir_imagemagick: %"/x/UTILS/ImageMagick-6.8.0-Q16/"
 ;It's used by imagick to load MagickWand's dll or the convert app
 
 do %rs.r
 print "RS ready"
-
+rs/run 'wav-to-mp3
+rs/run 'xfl-remove-duplicates
+rs/run 'xfl-shapes-to-symbols
+print {
+	xfl-shapes-to-symbols
+	xfl-remove-duplicates
+	wav-to-mp3 %
+	wmc
+}
 halt
