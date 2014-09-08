b2be001cfea4cd549e01b867e68785402adc8070
Oldes <oldes.huhuman@gmail.com>
2014-09-08 12:11:37 +0200

    Triangulator - slow version, which is focusing on size reduction by reusing vertices in vertesBuffers.

---
+++ projects-rswf/triangulator/shrink/triangulator.r
@@ -0,0 +1,429 @@
+REBOL [
+    Title: "Triangulator"
+    Date: 29-Dec-2013/16:35:22+1:00
+    Author: "Oldes"
+	require: [
+		rs-project %stream-io
+	]
+]
+
+ctx-triangulator: context [
+	out: make stream-io [] ;Holds output stream
+	;#CURVE RELATED FUNCTIONS
+	;used to convert Bazier curve to lines 
+	_terms: none
+	_subSteps: 0
+	_bezierError: 0.75
+	_minLineWidth: 20 ;in TWIPS!! so 40 = 2px
+	
+	_curveOutput: copy []
+
+	vertice-struct: make struct! [
+		x [float]
+		y [float]
+		r [float]
+		g [float]
+		b [float]
+		a [float]
+	] none
+	
+	quadraticCurve: func[
+		a1x   [number!]
+		a1y   [number!]
+		cx    [number!]
+		cy    [number!]
+		a2x   [number!]
+		a2y   [number!]
+		;error [decimal!]
+	][
+		_subSteps: 0
+		;_bezierError: error
+		_terms: reduce [a1x a1y cx cy a2x a2y]
+			
+		clear _curveOutput
+			
+		subdivide 0.0 0.5 0 :quadratic _curveOutput
+		subdivide 0.5 1.0 0 :quadratic _curveOutput
+		
+		;print _subSteps
+		head _curveOutput
+	]
+	quadratic: func[t axis][ 
+		oneMinusT: 1.0 - t
+		a1: _terms/(1 + axis)
+		c:  _terms/(3 + axis)
+		a2: _terms/(5 + axis)
+		(oneMinusT * oneMinusT * a1) + (2.0 * oneMinusT * t * c) + (t * t * a2)
+	]
+	subdivide: func[
+		t0 t1 depth equation _curveOutput
+		/local quadX quadY x0 y0 x1 y1 midX midY dx dy error2
+	][
+		quadX: equation (t0 + t1) * 0.5 0
+		quadY: equation (t0 + t1) * 0.5 1
+		
+		x0: equation t0 0
+		y0: equation t0 1
+		x1: equation t1 0
+		y1: equation t1 1
+		
+		midX: ( x0 + x1 ) * 0.5;
+		midY: ( y0 + y1 ) * 0.5;
+		
+		dx:  quadX - midX;
+		dy:  quadY - midY;
+		
+		error2: (dx * dx) + (dy * dy)
+
+		either error2 > (_bezierError * _bezierError) [
+			subdivide  t0 (t0 + t1) * 0.5 depth + 1 :equation _curveOutput	
+			subdivide (t0 + t1) * 0.5 t1  depth + 1 :equation _curveOutput	
+		][
+			_subSteps: _subSteps + 1
+			repend _curveOutput [x1 y1]
+		]
+	]
+	;END OF CURVE RELATED FUNCTIONS
+	
+	
+	
+	penPosX: penPosY: 0
+
+	strokeDefinition: copy []
+	vertexBuffers:    copy [] 
+	indexBuffers:     copy [] 
+	vertices:         make binary! 70000 ;-- array of vertices in binary format
+	indices:          copy [] ;-- array of indexes pointing to vertices
+	newIndices:       copy [] ;-- this is used as a temp holder, when buffer is full
+	vertToIndHash:    make hash! 65535
+	numVertices: 0
+	ind0: ind1: ind2: ind3: 0
+	idx: 0
+	
+	lineWidth: 1
+	lineColor: 255.255.255.255
+
+	stroke-to-polyline: func[
+		/local
+		v0x v0y v1x v1y v2x v2y 
+		d0 d1 d0x d0y d1x d1y
+		n0x n0y n1x n1y cnx cny c
+		c1xPos c1yPos c1xNeg c1yNeg
+		elbowThickness dot
+		clrR clrG clrB clrA
+		pos isFirst? isLast?
+		
+	][
+		strokeDefinition: head strokeDefinition
+		;prin "stroke-to-polyline" probe strokeDefinition
+		v0x: v0y:  none
+		parse strokeDefinition [
+			any [
+				'width set lineWidth number!
+				|
+				'color set lineColor tuple! (
+					clrR: lineColor/1 / 255
+					clrG: lineColor/2 / 255
+					clrB: lineColor/3 / 255
+					clrA: lineColor/4 / 255
+				)
+				|
+				set v1x number!
+				set v1y number!
+				pos: (
+					if isFirst?: none? v0x [
+						v0x: v1x
+						v0y: v1y
+					]
+					if isLast?: not parse pos [
+						opt ['width number!]
+						opt ['color tuple!]
+						set v2x number!
+						set v2y number!
+						to end
+					][
+						v2x: v1x
+						v2y: v1y
+					]
+					
+					d0x: v1x - v0x
+					d0y: v1y - v0y
+					d1x: v2x - v1x
+					d1y: v2y - v1y
+					
+					if isLast? [
+						v2x: v2x + d0x
+						v2y: v2y + d0y
+						d1x: v2x - v1x
+						d1y: v2y - v1y
+					]
+					if isFirst? [
+						v0x: v0x - d1x
+						v0y: v0y - d1y
+						d0x: v1x - v0x
+						d0y: v1y - v0y
+					]
+					d0: square-root (d0x * d0x) + (d0y * d0y)
+					d1: square-root (d1x * d1x) + (d1y * d1y)
+					
+					elbowThickness: lineWidth * 0.5
+
+					unless any [isFirst? isLast?][
+						;// Thanks to Tom Clapham for spotting this relationship.
+						dot: ((d0x * d1x) + (d0y * d1y)) / (d0 * d1)
+						either error? try [
+							elbowThickness: elbowThickness / sine/radians ((PI - arccosine/radians dot) * 0.5)
+						][
+							elbowThickness: lineWidth * 0.5
+						][
+							if elbowThickness > (lineWidth * 4) [
+								elbowThickness: lineWidth * 4
+							]
+						]
+					]
+					
+					n0y:  d0x / d0
+					n1x: - d1y / d1
+					n0x: - d0y / d0
+					n1y:  d1x / d1
+								  
+					cnx: n0x + n1x
+					cny: n0y + n1y
+					error? try [
+						c:   (1 / square-root( (cnx * cnx) + (cny * cny) )) * elbowThickness
+						cnx: cnx * c
+						cny: cny * c
+					]
+					
+					v1xPos: v1x + cnx
+					v1yPos: v1y + cny
+					v1xNeg: v1x - cnx
+					v1yNeg: v1y - cny
+							
+					vertice-struct/x: v1xPos
+					vertice-struct/y: v1yPos
+					vertice-struct/r: clrR
+					vertice-struct/g: clrG
+					vertice-struct/b: clrB
+					vertice-struct/a: clrA
+
+					vertBin: third vertice-struct
+					unless ind2: select vertToIndHash vertBin [
+						ind2: numVertices
+						insert vertToIndHash reduce [copy vertBin ind2]
+						insert tail vertices vertBin
+						numVertices: numVertices + 1
+					]
+
+					vertice-struct/x: v1xNeg
+					vertice-struct/y: v1yNeg
+					vertice-struct/r: clrR
+					vertice-struct/g: clrG
+					vertice-struct/b: clrB
+					vertice-struct/a: clrA
+
+					vertBin: third vertice-struct
+					unless ind3: select vertToIndHash vertBin [
+						ind3: numVertices
+						insert vertToIndHash reduce [copy vertBin ind3]
+						insert tail vertices vertBin
+						numVertices: numVertices + 1
+					]
+					unless isFirst? [
+						repend indices [ind0 ind2 ind1 ind1 ind2 ind3]
+					]
+					ind0: ind2
+					ind1: ind3
+
+					;store current point for future use
+					v0x: v1x
+					v0y: v1y
+				)
+			]	
+		]
+		strokeDefinition: clear head strokeDefinition
+	]
+	
+	set 'triangulate-shape func[
+		data
+		/local
+			lineWidth lineColor lineData curveData err
+			penPosX penPosY
+			startIdx newHash vertBin
+	][
+		;print ["triangulate-shape" mold copy/part data 10 "..."]
+		startIdx: length? indices
+		startVert: numVertices
+		
+		parse/all data [
+			any [
+				'lineStyle
+					set lineWidth integer! (
+						if lineWidth < _minLineWidth [lineWidth: _minLineWidth]
+						strokeDefinition: insert strokeDefinition reduce ['width lineWidth: lineWidth * 0.05]
+					)
+					opt [set lineColor tuple! (
+						strokeDefinition: insert strokeDefinition reduce ['color lineColor]			
+					)]
+				|
+				'moveTo (stroke-to-polyline) set penPosX number! set penPosY number! (
+					penPosX: penPosX * 0.05
+					penPosY: penPosY * 0.05
+					strokeDefinition: insert strokeDefinition reduce ['width lineWidth 'color lineColor penPosX penPosY]
+				)
+				|
+				'line set lineData block! (
+					if none? penPosX [
+						penPosX: 0
+						penPosY: 0
+						strokeDefinition: insert strokeDefinition reduce ['width lineWidth 'color lineColor penPosX penPosY]
+					]
+					foreach [dx dy] lineData [
+						strokeDefinition: insert strokeDefinition reduce [
+							penPosX: penPosX + (dx * 0.05)
+							penPosY: penPosY + (dy * 0.05)
+						]
+					]
+				)
+				|
+				'curve set curveData block! (
+					if none? penPosX [
+						penPosX: 0
+						penPosY: 0
+						strokeDefinition: insert strokeDefinition reduce ['width lineWidth 'color lineColor penPosX penPosY]
+					]
+					foreach [cx cy dx dy] curveData [
+						strokeDefinition: insert strokeDefinition quadraticCurve
+							penPosX penPosY
+							penPosX: penPosX + (cx * 0.05)
+							penPosY: penPosY + (cy * 0.05)
+							penPosX: penPosX + (dx * 0.05)
+							penPosY: penPosY + (dy * 0.05)
+						
+						
+					]
+				)
+				| copy err 1 skip (
+					ask reform ["Invalid shape definition:" mold err]
+				)
+			]
+			(stroke-to-polyline)
+		]
+		;print ["vertices: " numVertices]
+		;probe new-line/skip to block! vertToIndHash true 2
+		if numVertices > 65535 [;65535
+			print ["BufferFull" length? vertices "startIdx: " startIdx " triangles: " ((length? indices) - startIdx) / 3 ]
+
+
+			append/only indexBuffers  copy/part indices startIdx
+			append/only vertexBuffers copy/part vertices (startVert * 24)
+
+			indices: skip indices startIdx
+			clear vertices
+
+			newHash: make hash! 20000
+			numVertices: startIdx: startVert: 0
+			;probe new-line/skip to block! vertToIndHash true 2
+			foreach ind indices [
+				vertBin: first back find vertToIndHash ind
+				unless ind: select newHash vertBin [
+					ind: numVertices
+					insert newHash reduce [copy vertBin ind]
+					insert tail vertices vertBin
+					numVertices: numVertices + 1
+				]
+				insert tail newIndices ind
+			]
+
+			indices: clear head indices
+			insert indices newIndices
+			clear newIndices
+			insert clear vertToIndHash newHash
+		]
+
+		reduce [
+			length? vertexBuffers                  ;-- indexBuffer
+			startIdx                               ;-- firstIndex
+			((length? indices) - startIdx) / 3     ;-- numTriangles
+		]
+	]
+
+	init: does [
+		out/clearBuffers
+		forall vertexBuffers [clear vertexBuffers/1]
+		forall indexBuffers  [clear indexBuffers/1]
+		vertexBuffers: clear head vertexBuffers
+		indexBuffers:  clear head indexBuffers
+		vertices: clear head vertices
+		indices:  clear head indices
+		clear vertToIndHash
+		ind0: ind1: ind2: ind3: 0
+	]
+
+	get-buffers-binary: func[][
+		out/clearBuffers
+		
+		append/only indexBuffers  copy indices
+		append/only vertexBuffers copy vertices
+		;probe vertexBuffers
+		;probe indexBuffers
+
+		either empty? vertexBuffers/1 [
+			out/writeUI8 0
+		][
+			out/writeUI8 length? vertexBuffers
+			;print ["NUM BUFFERS:" length? vertexBuffers]
+			foreach vertices vertexBuffers [
+				out/writeUI32 (length? vertices) / 24
+				print ["VERTEX BUFFER BYTES: " length? vertices]
+				out/writeBytes vertices
+				clear head vertices
+			]
+			;shapes indexBuffer:
+			foreach indices indexBuffers [
+				out/writeUI32 length? indices
+				print ["INDEXE BUFFERs: " length? indices]
+				forall indices [out/writeUI16 indices/1]
+				clear head indices
+			]
+		]
+		clear vertexBuffers
+		clear indexBuffers
+		;probe copy/part head out/outBuffer 4
+		head out/outBuffer
+	]	
+{
+	triangulate-shape [
+		lineStyle 20 255.0.0.255 
+		moveTo 200 0 
+		line [2000 0]
+	]
+
+	triangulate-shape [
+		;lineStyle 20 0.0.255.255 
+		moveTo 200 0 
+		line [0 2000 2000 2000]
+	]
+	
+;	append/only indexBuffers  copy indices
+;		append/only vertexBuffers copy vertices
+	
+	probe get-buffers-binary
+
+		
+	probe idx
+	;probe vertices
+	out: copy ""
+	foreach [x y r g b a] vertices [
+		append out ajoin [x ", " y ",  " r ", " g ", " b ", " a ", " lf] 
+	]
+	append out "^/^/"
+	n: 0
+	foreach [i1 i2 i3] indices [
+		if n = 2 [n: 0 append out "^/"]
+		append out ajoin [i1 "," i2 "," i3 ", "]
+		n: n + 1
+	]
+	print out
+}
+]
